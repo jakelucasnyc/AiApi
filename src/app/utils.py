da30ecf0a16a46eb0fcc96aa1c71a74e148dc1b7
jakelucasnyc <jakelucasnyc@gmail.com>
2023-06-15 12:16:19 -0400

    more tweaking

--- src/app/utils.py
+++ src/app/utils.py
@@ -17,21 +17,21 @@ import deepspeed
 
 def load_starcoder():
     checkpoint = 'HuggingFaceH4/starchat-beta'
-    # config = BitsAndBytesConfig(
-    #     load_in_4bit=True,
-    #     bnb_4bit_quant_type='nf4',
-    #     bnb_4bit_use_double_quant=True,
-    #     bnb_4bit_compute_dtype=torch.bfloat16,
-    # )
+    config = BitsAndBytesConfig(
+        load_in_4bit=True,
+        bnb_4bit_quant_type='nf4',
+        bnb_4bit_use_double_quant=True,
+        bnb_4bit_compute_dtype=torch.bfloat16,
+    )
     _logger.info('Loading model...')
     start = time.perf_counter()
     model = AutoModelForCausalLM.from_pretrained(checkpoint, 
                                                  device_map='auto', 
-                                                #  torch_dtype=torch.bfloat16,
-                                                #  quantization_config=config, 
-                                                 load_in_8bit=True,
+                                                 torch_dtype=torch.bfloat16,
+                                                 quantization_config=config, 
+                                                #  load_in_8bit=True,
 
-                                                #  local_files_only=True
+                                                 local_files_only=True
                                                  )
     # model.eval()
     model = deepspeed.init_inference(model,
