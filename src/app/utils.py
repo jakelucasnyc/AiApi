9b4a7a3af9d118640a61dc4621134982673ea2e1
jakelucasnyc <jakelucasnyc@gmail.com>
2023-06-15 13:11:20 -0400

    removing deepspeed for testing

--- src/app/utils.py
+++ src/app/utils.py
@@ -34,13 +34,13 @@ def load_starcoder():
                                                  local_files_only=True
                                                  )
     # model.eval()
-    model = deepspeed.init_inference(model,
-                                    #  mp_size=1,
-                                     dtype=torch.int8,
-                                    #  replace_method='auto',
-                                     replace_with_kernel_inject=True,
-                                     enable_cuda_graph=True,
-                                     )
+    # model = deepspeed.init_inference(model,
+    #                                 #  mp_size=1,
+    #                                  dtype=torch.int8,
+    #                                 #  replace_method='auto',
+    #                                  replace_with_kernel_inject=True,
+    #                                  enable_cuda_graph=True,
+    #                                  )
     tokenizer = AutoTokenizer.from_pretrained(checkpoint, model_max_length=7500, device_map='auto')
     tokenizer.pad_token = '<|pad|>'
     elapsed = time.perf_counter() - start
