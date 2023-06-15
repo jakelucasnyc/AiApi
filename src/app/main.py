562dc5e2c2c24353abf3cb1716382084520e6ded
jakelucasnyc <jakelucasnyc@gmail.com>
2023-06-14 22:30:37 -0400

    trying regular deepspeed

--- src/app/main.py
+++ src/app/main.py
@@ -5,20 +5,20 @@ from fastapi import FastAPI, HTTPException
 from utils import load_starcoder
 from models import Prompt
 import logging
-from mii_utils import generator
+# from mii_utils import generator
 
 _logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
 app = FastAPI()
-# app.model = None
-# app.tokenizer = None
-# app.model, app.tokenizer = load_starcoder()
+app.model = None
+app.tokenizer = None
+app.model, app.tokenizer = load_starcoder()
 
 @app.post('/prompt/', status_code=200)
 def prompt(prompt: Prompt):
-    # if app.model is None or app.tokenizer is None:
-    #     raise HTTPException(status_code=503, detail='StarChat model and/or tokenizer not initialized')
+    if app.model is None or app.tokenizer is None:
+        raise HTTPException(status_code=503, detail='StarChat model and/or tokenizer not initialized')
     if prompt.temp <= 0 or prompt.temp > 1:
         raise HTTPException(status_code=400, detail=f'Temperature must be in this range: 0 <= temp >= 1, not {prompt.temp}')
 
@@ -27,37 +27,38 @@ def prompt(prompt: Prompt):
         prompt_string = f"<|system|>\n{prompt.system}<|end|>\n<|user|>\n{user_prompt}<|end|>\n<|assistant|>"
         prompt_strings.append(prompt_string)
 
-    outputs = generator.query({'query': prompt_strings}, 
-                              max_new_tokens=500, 
-                              do_sample=True, 
-                              temperature=prompt.temp, 
-                              top_k=50, 
-                              top_p=0.95, 
-                              eos_token_id=49155, 
-                              pad_token_id=49155, 
-                              )
-    print(outputs.response)
-    # tokenized = app.tokenizer(prompt_strings, return_tensors='pt', padding=True)
-    # input_ids = tokenized.input_ids
-    # input_ids = input_ids.to(app.model.device)
+    # outputs = generator.query({'query': prompt_strings}, 
+    #                           max_new_tokens=500, 
+    #                           do_sample=True, 
+    #                           temperature=prompt.temp, 
+    #                           top_k=50, 
+    #                           top_p=0.95, 
+    #                           eos_token_id=49155, 
+    #                           pad_token_id=49155, 
+    #                           )
+    # print(outputs.response)
+    tokenized = app.tokenizer(prompt_strings, return_tensors='pt')
+    input_ids = tokenized.input_ids
+    input_ids = input_ids.to(app.model.device)
 
-    # if len(input_ids[0]) > app.tokenizer.model_max_length:
-    #     raise HTTPException(status_code=400, detail=f'Prompt must be under {app.tokenizer.model_max_length} tokens, not {len(input_ids[0])}')
+    if len(input_ids[0]) > app.tokenizer.model_max_length:
+        raise HTTPException(status_code=400, detail=f'Prompt must be under {app.tokenizer.model_max_length} tokens, not {len(input_ids[0])}')
 
-    # # print(inputs)
-    # # print(len(inputs))
-    # _logger.info('Running inference...')
-    # start = time.perf_counter()
-    # outputs = app.model.generate(input_ids=input_ids, 
-    #                             #  return_dict_in_generate=True,
-    #                              max_new_tokens=500, 
-    #                              do_sample=True, 
-    #                              temperature=prompt.temp, 
-    #                              top_k=50, 
-    #                              top_p=0.95, 
-    #                              eos_token_id=49155, 
-    #                              attention_mask=tokenized.attention_mask,
-    #                              )
-    # elapsed = time.perf_counter()-start
-    # _logger.info(f'Ran inference ({elapsed: .3f}s)')
+    # print(inputs)
+    # print(len(inputs))
+    _logger.info('Running inference...')
+    start = time.perf_counter()
+    outputs = app.model.generate(input_ids=input_ids, 
+                                #  return_dict_in_generate=True,
+                                 max_new_tokens=500, 
+                                 do_sample=True, 
+                                 temperature=prompt.temp, 
+                                 top_k=50, 
+                                 top_p=0.95, 
+                                 eos_token_id=49155, 
+                                 attention_mask=tokenized.attention_mask,
+                                 )
+    elapsed = time.perf_counter()-start
+    _logger.info(f'Ran inference ({elapsed: .3f}s)')
     # return {f'r{i+1}': output for i, output in enumerate(app.tokenizer.batch_decode(outputs, clean_up_tokenization_spaces=False))}
+    return {'response': app.tokenizer.decode(outputs[0], clean_up_tokenization_spaces=False)}
--- src/app/utils.py
+++ src/app/utils.py
@@ -4,6 +4,7 @@ import logging
 _logger = logging.getLogger(__name__)
 import torch
 import time
+import deepspeed
 
 # def _load_starcoder():
 #     checkpoint = 'HuggingFaceH4/starchat-beta'
@@ -16,23 +17,27 @@ import time
 
 def load_starcoder():
     checkpoint = 'HuggingFaceH4/starchat-beta'
-    config = BitsAndBytesConfig(
-        load_in_4bit=True,
-        bnb_4bit_quant_type='nf4',
-        bnb_4bit_use_double_quant=True,
-        bnb_4bit_compute_dtype=torch.bfloat16,
-    )
+    # config = BitsAndBytesConfig(
+    #     load_in_4bit=True,
+    #     bnb_4bit_quant_type='nf4',
+    #     bnb_4bit_use_double_quant=True,
+    #     bnb_4bit_compute_dtype=torch.bfloat16,
+    # )
     _logger.info('Loading model...')
     start = time.perf_counter()
     model = AutoModelForCausalLM.from_pretrained(checkpoint, 
-                                                 device_map='auto', 
+                                                #  device_map='auto', 
                                                 #  torch_dtype=torch.bfloat16
-                                                 quantization_config=config, 
+                                                #  quantization_config=config, 
 
                                                 #  local_files_only=True
                                                  )
-    model.eval()
-    tokenizer = AutoTokenizer.from_pretrained(checkpoint, model_max_length=7500, device_map='auto')
+    # model.eval()
+    model = deepspeed.init_inference(model,
+                                     mp_size=1,
+                                     dtype=torch.bfloat16,
+                                     replace_with_kernel_inject=True)
+    tokenizer = AutoTokenizer.from_pretrained(checkpoint, model_max_length=7500)
     tokenizer.pad_token = '<|pad|>'
     elapsed = time.perf_counter() - start
     _logger.info(f'Loaded model ({elapsed: .3f}s)')
