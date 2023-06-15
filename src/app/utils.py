ee7b16b24d99cba14f8416bd810d42deda70710b
jakelucasnyc <jakelucasnyc@gmail.com>
2023-06-14 20:28:31 -0400

    added double quant

--- src/app/utils.py
+++ src/app/utils.py
@@ -19,7 +19,7 @@ def load_starcoder():
     config = BitsAndBytesConfig(
         load_in_4bit=True,
         bnb_4bit_quant_type='nf4',
-        bnb_4bit_use_double_quant=False,
+        bnb_4bit_use_double_quant=True,
         bnb_4bit_compute_dtype=torch.bfloat16,
     )
     _logger.info('Loading model...')
