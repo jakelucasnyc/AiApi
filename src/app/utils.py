0c9ae531ffc3183a8657611b4550304bedf82413
jakelucasnyc <jakelucasnyc@gmail.com>
2023-06-14 20:22:53 -0400

    added pad_token_id

--- src/app/utils.py
+++ src/app/utils.py
@@ -24,7 +24,11 @@ def load_starcoder():
     )
     _logger.info('Loading model...')
     start = time.perf_counter()
-    model = AutoModelForCausalLM.from_pretrained(checkpoint, device_map='auto', quantization_config=config, local_files_only=True)
+    model = AutoModelForCausalLM.from_pretrained(checkpoint, 
+                                                 device_map='auto', 
+                                                 quantization_config=config, 
+                                                #  local_files_only=True
+                                                 )
     model.eval()
     tokenizer = AutoTokenizer.from_pretrained(checkpoint, model_max_length=7500, device_map='auto', local_files_only=True)
     elapsed = time.perf_counter() - start
